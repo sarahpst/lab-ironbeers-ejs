85447e6ce0aae7663d0920d39178e6a3037b7751
sarahpst <111440718+sarahpst@users.noreply.github.com>
2023-04-18 18:20:41 +0200

    all iterations without bonus with Olivier

--- app.js
+++ app.js
@@ -18,5 +18,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => res.render('random-beer', {responseFromAPI}))
+  .catch(error => console.log(error));
+});
+
+app.get('/partial/beer', (req, res) => {
+  res.render('beer');
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
